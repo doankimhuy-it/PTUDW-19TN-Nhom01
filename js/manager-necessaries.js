45b13bf724adab535340609c38f675a7c62cd5d3
doankimhuy-it <doankimhuy.it@gmail.com>
2022-04-24 14:16:03 +0700

    Correct spelling mistakes

--- js/manager-necessaries.js
+++ js/manager-necessaries.js
@@ -1,8 +1,8 @@
 function switchGroupToProduct() {
     var GroupIDBtn = document.getElementById("sp-left-group-btn")
     var ProductIDBtn = document.getElementById("sp-left-product-btn")
-    var groupTable = document.getElementById("neccessary-group")
-    var productTable = document.getElementById("neccessary-product")
+    var groupTable = document.getElementById("necessaries-group")
+    var productTable = document.getElementById("necessaries-product")
     GroupIDBtn.style.display = 'none';
     groupTable.style.display = 'none';
     ProductIDBtn.style.display = 'block';
@@ -12,8 +12,8 @@ function switchGroupToProduct() {
 function switchProductToGroup() {
     var ProductIDBtn = document.getElementById("sp-left-product-btn")
     var GroupIDBtn = document.getElementById("sp-left-group-btn")
-    var groupTable = document.getElementById("neccessary-group")
-    var productTable = document.getElementById("neccessary-product")
+    var groupTable = document.getElementById("necessaries-group")
+    var productTable = document.getElementById("necessaries-product")
     GroupIDBtn.style.display = 'block';
     groupTable.style.display = 'block';
     ProductIDBtn.style.display = 'none';
